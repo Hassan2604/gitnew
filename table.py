004fef8d6ed57ae3a573d5dfbf0018bc6dd248d3
Hassan2604 <2223180@ncbae.edu.pk>
2023-10-24 21:46:16 +0500

    added new program

--- table.py
+++ table.py
@@ -5,3 +5,10 @@ def get_user_input(text):
             return user_input
         except ValueError:
             print("Invalid input, please try again.")
+
+while True:
+    user_input = get_user_input("Enter your input: ")    
+    if user_input == "exit":
+        break
+    for i in range(1, 11):
+        print(f"{user_input} x {i} = {int(user_input) * i}")
