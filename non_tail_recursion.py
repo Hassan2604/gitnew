b2ab47253c8c8622eaabfa026e98129b53a9f395
hassan2604 <2223180@ncbae.edu.pk>
2023-11-17 09:31:04 -0800

    added a new file

--- non_tail_recursion.py
+++ non_tail_recursion.py
@@ -8,10 +8,8 @@ def factorial_non_tail_recursive(n):
             result *= n
         return result
 
-def linear_recursion(n):
-    if n == 0:
-        return 1
-    elif n == 1:
-        return 1
-    else:
-        return linear_recursion(n - 1) + linear_recursion(n - 2)
+# Get input from user
+n = int(input("Enter a number: "))
+print(factorial_non_tail_recursive(n))
+
+
---
+++ toss.py
@@ -0,0 +1,7 @@
+import random
+my_list = [1, 2]
+random_element = random.choice(my_list)
+if random_element == 1:
+    print("Heads")
+else: 
+    print("Tails")
